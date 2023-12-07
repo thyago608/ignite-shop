eb8e55dae1c6b7acf9b2e00d8ef8191185ccdb8d
@thyago608 <thyagoribeiro608@gmail.com>
2023-12-07 10:49:20 -0300

    feat(Home): Create Styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { styled } from '../styles'
+import { HomeContainer, Product } from '../styles/pages/home'
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+
 
 export default function Home() {
   return (
-    <h2>Hello World</h2>
-  )
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+    )
 }
